2c6fa46564598d58941d3bc7b305df4ee84f7b50
Tonitch <minecraftonitch@gmail.com>
2020-06-07 21:28:25 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+
+
+class Bot_Planing(discord.Client):
+    async def on_ready(self):
+        print(f"{self.user} is ready!")
+
+
+def main():
+    app = Bot_Planing()
+    app.run(open("key.txt").read())
+
+
+if __name__ == "__main__":
+    main()
