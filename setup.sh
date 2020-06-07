c619f8163c8c816afa666fae7635c4092f2e7a89
Tonitch <minecraftonitch@gmail.com>
2020-06-08 01:04:17 +0200

    adding cal command + salt in setup

--- setup.sh
+++ setup.sh
@@ -7,8 +7,11 @@ cp botplanning.service /etc/systemd/system/
 
 echo "Entrez le token du bot (ctrl-shift-v): "
 read key
+echo "Entrez le salt utilisé par la page web: "
+read salt
 
 echo $key > key.txt
+echo $salt > salt.txt
 
 cp -r ./ /usr/local/src/BotPlanning/
 cd /usr/local/src/BotPlanning
