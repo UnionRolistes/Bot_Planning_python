024b2367346a46c8de319c67be06b8b9a25169c8
Lyss <shaitan.lyssm@gmail.com>
2021-07-29 03:38:24 +0200

    Change Calendar.creators_to_webhook

--- src/cog_planning/cog.py
+++ src/cog_planning/cog.py
@@ -77,9 +77,10 @@ class Planning(urpy.MyCog):
                     # Try to get webhook
                     webhooks = await anncmnt_channel.webhooks()
                     webhook: discord.Webhook = webhooks[0]
-                    Calendar.creators_to_webhook[ctx.author.id] = webhook
-                    with open('/tmp/cal', 'wb') as f:
+                    Calendar.creators_to_webhook[ctx.author.id] = (webhook.url, webhook.guild_id, webhook.channel_id)
+                    with open("/tmp/cal", "wb") as f:
                         pickle.dump(Calendar.creators_to_webhook, f)
+
                 except discord.errors.Forbidden:
                     # Insufficient permissions
                     error_log("Impossible d'obtenir les webhooks.",
