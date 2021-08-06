c24eee9255eec9c3c080d92661fcac2ed98edb57
Youssef <agoussalyo@eisti.eu>
2021-08-06 12:12:55 +0200

    Quick fix

--- src/cog_planning/settings.py
+++ src/cog_planning/settings.py
@@ -1,5 +1,5 @@
 command_prefix = "$"
-creation_form_url = 'http://planning.unionrolistes.fr/'
+creation_form_url = 'http://planning.unionrolistes.fr'
 calendar_url = 'http://planning.unionrolistes.fr/Calendar'
 announcement_channel = 'planning-jdr'
 msg_delete_delay = 6
